23377d32639b800ee923e94290d1a303e0b63394
Remy Dinh <remy_dinh@yahoo.com>
2021-05-26 04:34:56 -0700

    Added intro() and mainMenu() to main(), but no real functionality yet.

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -4,8 +4,85 @@
 // Prepared by: Remy Dinh
 
 #include <iostream>
+#include <fstream>  // We are reading from a file after all.
 
-int main (void)
+void intro();
+char mainMenu();
+
+int main()
 {
+    intro();
+    char userChoice = '';
+    while (userChoice != 'X' && userChoice != 'x')
+    {
+        userChoice = mainMenu();
+        // Then execute the respective selected option below.
+    }
     return 0;
+}
+
+void intro()
+{
+    std::cout << std::endl;
+    std::cout << "######################################################" << std::endl;
+    std::cout << "#   CIS 22C Data Structures Team Project: Group 2    #" << std::endl;
+    std::cout << "######################################################" << std::endl;
+    std::cout << std::endl;
+    std::cout << "------------------------------------------------------" << std::endl;
+    std::cout << "                    Group Members                     " << std::endl;
+    std::cout << "------------------------------------------------------" << std::endl;
+    std::cout << "    Remy Dinh   : Project Coordination & Integration"   << std::endl;
+    std::cout << "    Shun Furuya : Hash List Algorithms"                 << std::endl;
+    std::cout << "    Mahik Kaur  : Screen Output"                        << std::endl;
+    std::cout << "    Taeyoon Kim : BST Algorithms"                       << std::endl;
+    std::cout << "------------------------------------------------------" << std::endl;
+    std::cout << std::endl;
+    std::cout << "######################################################" << std::endl;
+    std::cout << "#                                                    #" << std::endl;
+    std::cout << "#             European Country Database              #" << std::endl;
+    std::cout << "#                                                    #" << std::endl;
+    std::cout << "######################################################" << std::endl;
+    std::cout << "######################################################" << std::endl;
+    std::cout << "#                                                    #" << std::endl;
+    std::cout << "#        ====                                        #" << std::endl;
+    std::cout << "#        |                                           #" << std::endl;
+    std::cout << "#        |==  |  | r== === === === === | |           #" << std::endl;
+    std::cout << "#        |    |  | |   | | | | |=  |=| |N|           #" << std::endl;
+    std::cout << "#        ===+ ==== |   === === === | | | |           #" << std::endl;
+    std::cout << "#                          |                         #" << std::endl;
+    std::cout << "#                          |                         #" << std::endl;
+    std::cout << "#        ===                                         #" << std::endl;
+    std::cout << "#        |   === | | | | === r== === === (-/         #" << std::endl;
+    std::cout << "#        |   | | | | |N|  |  |    |  |=   S          #" << std::endl;
+    std::cout << "#        === === === | |  |  |   === === /-)         #" << std::endl;
+    std::cout << "#                                                    #" << std::endl;
+    std::cout << "#                                                    #" << std::endl;
+    std::cout << "#         D=                                         #" << std::endl;
+    std::cout << "#         | |  A  ===  A  |=)  A  (^/ ===            #" << std::endl;
+    std::cout << "#         | | |_|  |  |_| |<  |-|  S  |-             #" << std::endl;
+    std::cout << "#         D=  | |  |  | | |=) | | /-) ===            #" << std::endl;
+    std::cout << "#                                                    #" << std::endl;
+    std::cout << "######################################################" << std::endl;
+    std::cout << std::endl;
+}
+
+void mainMenu()
+{
+    std::cout << "----------" << std::endl;
+    std::cout << "Main Menu:" << std::endl;
+    std::cout << "----------" << std::endl;
+
+    std::cout << "A. Add new data"               << std::endl;
+    std::cout << "B. Delete data"                << std::endl; // Need to figure out how this will work
+    std::cout << "C. Search by country name"     << std::endl; // Primary key, handled by Hash Table
+    std::cout << "D. Search by language"         << std::endl; // Secondary key, display all matches (BST)
+    std::cout << "E. List countries by language" << std::endl; // Print inOrder traversal of the BST
+    std::cout << "F. Write data to file"         << std::endl; // File I/O baby.
+    std::cout << "G. Hash Table Statistics"      << std::endl; // Print # of collisions, longest collision list, load factor, etc.
+    std::cout << "H. Display Menu"               << std::endl; // Show this menu once after start, then only upon request.
+    std::cout << "X. Exit"                       << std::endl;
+    
+    // Hidden menu options
+    // 1. Display indented tree
+    // 2. Display names of team members lol
 }
